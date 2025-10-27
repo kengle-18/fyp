16cd28d7684b685209cdc82c183bb4f2df8e44a1
kengle-18 <127601339+kengle-18@users.noreply.github.com>
2025-10-27 22:33:55 +0800

    scala grpc for int and boolean ok

--- scala/src/main/scala/grpcHelper.scala
+++ scala/src/main/scala/grpcHelper.scala
@@ -9,10 +9,10 @@ import org.slf4j.LoggerFactory
 import com.example.base._ // adjust based on the generated folder
 
 // 1️⃣ Server Implementation
-class GreeterImpl extends GreeterGrpc.Greeter {
+class UniversalTesterImpl extends UniversalTesterGrpc.UniversalTester {
 
-  override def sayHello(request: HelloRequest): Future[HelloReply] =
-    Future.successful(HelloReply(message = s"Hello, ${request.name}!"))
+  override def sendUniversal(request: UniversalMessage): Future[UniversalMessage] =
+    Future.successful(request) // simply echo back for testing
 }
 
 // 2️⃣ gRPC Server
@@ -20,7 +20,7 @@ class GrpcServer(executionContext: ExecutionContext, port: Int) {
 
   val server: Server = ServerBuilder
     .forPort(port)
-    .addService(GreeterGrpc.bindService(new GreeterImpl, executionContext))
+    .addService(UniversalTesterGrpc.bindService(new UniversalTesterImpl, executionContext))
     .build()
 
   def start(): Unit = {
@@ -40,14 +40,54 @@ class GrpcClient(host: String, port: Int) {
     .usePlaintext()
     .build()
 
-  val blockingStub: GreeterGrpc.GreeterBlockingStub =
-    GreeterGrpc.blockingStub(channel)
+  val blockingStub: UniversalTesterGrpc.UniversalTesterBlockingStub =
+    UniversalTesterGrpc.blockingStub(channel)
 
   def shutdown(): Unit = channel.shutdown()
 
-  def greet(name: String): Unit = {
-    val request = HelloRequest(name)
-    val response = blockingStub.sayHello(request)
-    println(s"Client: Greeting received: ${response.message}")
+  def sendMessage(fieldName: String, value: Any): Unit = {
+    val msgToSend = fieldName match {
+      case "singleInt"    => UniversalMessage(singleInt = Some(value.asInstanceOf[Int]))
+      case "singleString" => UniversalMessage(singleString = Some(value.asInstanceOf[String]))
+      case "singleBool"   => UniversalMessage(singleBool = Some(value.asInstanceOf[Boolean]))
+      case _              => throw new IllegalArgumentException(s"Unknown field: $fieldName")
+    }
+    val response: UniversalMessage = blockingStub.sendUniversal(msgToSend)
+    println(s"Sent field $fieldName with value $value, got response: $response")
   }
+
+  def sendAllMessages(message: UniversalMessage): Unit = {
+    val setFields = getSetFields(message)
+    setFields.foreach {
+      case (fieldName, value) =>
+        println(s"Sending field $fieldName with value $value")
+        sendMessage(fieldName, value)
+    }
+  }
+
+  def getSetFields(message: UniversalMessage): Seq[(String, Any)] =
+    message.productIterator
+      .zip(message.productElementNames)
+      .flatMap {
+        case (Some(v), name)                        => Some(name -> v) // Optional field is set
+        case (seq: Seq[_], name) if seq.nonEmpty    => Some(name -> seq) // Repeated field
+        case (map: Map[_, _], name) if map.nonEmpty => Some(name -> map) // Map field
+        case _                                      => None // Not set
+      }
+      .toSeq
+
+}
+
+object RuntimeOptionalUpdater {
+
+  // Update an optional Int field at runtime
+  def updateOptionalInt(msg: UniversalMessage, fieldName: String, value: Any): Either[String, UniversalMessage] =
+    fieldName match {
+      case "singleInt" =>
+        value match {
+          case v: Int => Right(msg.update(_.singleInt := v))
+          case _      => Left(s"Invalid type for $fieldName: ${value.getClass}")
+        }
+      case _ => Left(s"Unknown field: $fieldName")
+    }
 }
--- scala/src/main/scala/scalaClient.scala
+++ scala/src/main/scala/scalaClient.scala
@@ -32,17 +32,6 @@ object ScalaClient extends App {
     _.optionalSingleBool := Some(true)
   )
 
-  msg1.productIterator.zip(msg1.productElementNames).foreach {
-    case (value, name) =>
-      // println(value, name)
-      value match {
-        case Some(v)                        => println(s"$name is set: $v")
-        case seq: Seq[_] if seq.nonEmpty    => println(s"$name is set: $seq")
-        case map: Map[_, _] if map.nonEmpty => println(s"$name is set: $map")
-        case _                              => // field not set
-      }
-  }
-
-  try client.greet(name)
+  try client.sendAllMessages(msg1)
   finally client.shutdown()
 }
